3df18a595eeb1c447c5b746870cb07de4e7b996a
1adamh <hermann.adam@stud.uni-obuda.hu>
2023-11-18 19:51:10 +0100

    repository class-ok (taxi, ride, passenger)

---
+++ G365FF_HFT_2023241.Repository/Class/RideRepo.cs
@@ -0,0 +1,33 @@
+﻿using G365FF_HFT_2023241.Models;
+using G365FF_HFT_2023241.Repository.Data;
+using G365FF_HFT_2023241.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G365FF_HFT_2023241.Repository.Class
+{
+    public class RideRepo : Repository<Ride>, IRepository<Ride>
+    {
+        public RideRepo(TaxiDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Ride Read(int id)
+        {
+            return ctx.Rides.FirstOrDefault(x => x.RID == id);
+        }
+
+        public override void Update(Ride item)
+        {
+            var old = Read(item.RID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ G365FF_HFT_2023241.Repository/Class/TaxiRepo.cs
@@ -0,0 +1,33 @@
+﻿using G365FF_HFT_2023241.Models;
+using G365FF_HFT_2023241.Repository.Data;
+using G365FF_HFT_2023241.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G365FF_HFT_2023241.Repository.Class
+{
+    public class TaxiRepo : Repository<Taxi>, IRepository<Taxi>
+    {
+        public TaxiRepo(TaxiDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Taxi Read(int id)
+        {
+            return ctx.Taxis.FirstOrDefault(x => x.TID == id);       
+        }
+
+        public override void Update(Taxi item)
+        {
+            var old = Read(item.TID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
