633c3f62b47e9ae4f390448e505986bd248afaab
1adamh <hermann.adam@stud.uni-obuda.hu>
2023-11-18 19:51:10 +0100

    repository class-ok

---
+++ G365FF_HFT_2023241.Repository/Class/PassengerRepo.cs
@@ -0,0 +1,33 @@
+﻿using G365FF_HFT_2023241.Models;
+using G365FF_HFT_2023241.Repository.Data;
+using G365FF_HFT_2023241.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G365FF_HFT_2023241.Repository.Class
+{
+    public class PassengerRepo : Repository<Passenger>, IRepository<Passenger>
+    {
+        public PassengerRepo(TaxiDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Passenger Read(int id)
+        {
+            return ctx.Passengers.FirstOrDefault(x => x.PID == id);
+        }
+
+        public override void Update(Passenger item)
+        {
+            var old= Read(item.PID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
