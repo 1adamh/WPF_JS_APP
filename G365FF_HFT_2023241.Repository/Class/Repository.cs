074600459874f7fb834a7c89f86eec641d5bdd62
1adamh <hermann.adam@stud.uni-obuda.hu>
2023-11-18 19:48:29 +0100

    repo class, interface

---
+++ G365FF_HFT_2023241.Repository/Class/Repository.cs
@@ -0,0 +1,45 @@
+﻿using G365FF_HFT_2023241.Repository.Data;
+using G365FF_HFT_2023241.Repository.Interface;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G365FF_HFT_2023241.Repository.Class
+{
+    public abstract class Repository<T> : IRepository<T> where T: class  
+    {
+        protected TaxiDbContext ctx;
+        public Repository(TaxiDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+        
+            
+        
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract void Update(T item);
+        
+    }
+}
